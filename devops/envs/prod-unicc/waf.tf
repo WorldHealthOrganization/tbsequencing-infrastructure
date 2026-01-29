dbbd4bc4f641136fa2671c90aa1418dcc805414c
Sacha Laurent <sacha.laurent@finddx.org>
2026-01-29 18:08:03 +0100

    updating waf to v1.7

--- devops/envs/prod-unicc/waf.tf
+++ devops/envs/prod-unicc/waf.tf
@@ -1,5 +1,5 @@
 module "waf" {
-  source    = "git::git@bitbucket.org:awsopda/who-seq-treat-tbkb-terraform-modules.git//waf?ref=waf-v1.5"
+  source    = "git::git@bitbucket.org:awsopda/who-seq-treat-tbkb-terraform-modules.git//waf?ref=waf-v1.7"
   lb_arn    = module.alb.load_balancer_arn.lb
   cf_domain = var.cf_domain
 }
